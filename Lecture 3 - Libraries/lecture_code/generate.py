d7c32f4f14513af17fecefe4e23a5fc5e8b3f280
jnrslm3 <jumabekovn43@gmail.com>
2025-10-27 10:36:23 +0600

    some changes

--- Lecture 3 - Libraries/lecture_code/generate.py
+++
@@ -1,6 +0,0 @@
-import random
-
-cards = ["jack", "queen", "king"]
-random.shuffle(cards)
-for card in cards:
-    print(card)
--- Lecture 3 - Libraries/lecture_code/say.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-from sayings import goodbye
-
-if len(sys.argv) == 2:
-    goodbye(sys.argv[1])
